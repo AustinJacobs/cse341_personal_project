06cec6909c82c8e1b1946e5ed84022062493517a
Austin Jacobs <aust.t.jac@gmail.com>
2022-10-25 21:34:44 -0600

    Finished initial login and logout functionality

--- index.js
+++ index.js
@@ -1,24 +1,68 @@
 const express = require('express');
+const session = require('express-session');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongodb = require('./database/connect');
+const passport = require('passport');
+require('./authentication/auth');
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+app.use(session({ secret: process.env.SECRET }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+  res.send(
+    '<div style="height: 100vh; align-items: center; display: flex; justify-content: center;"><div style="font-family: sans-serif; background-color: #F0F0F0; height: 50px; border: 2px solid gray; border-radius: 10px; width: 200px; margin: 0 auto; align-items: center; display: flex; justify-content: center;"><a style="text-decoration: none" href="/auth/google">Authenticate with Google</a></div></div>'
+  );
+});
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] })
+);
+
+app.get(
+  '/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/api-docs',
+    failureRedirect: '/auth/failure',
+  })
+);
+
+app.get('/auth/failure', (req, res) => {
+  res.send('Authentication Failed');
+});
+
+// app.get('/protected', isLoggedIn, (req, res) => {
+//   res.send(`Hello! ${req.user.displayName}`);
+// });
+
+app.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.session.destroy();
+    res.redirect('/');
+  });
+});
 
 app
-  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  // .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(cors())
   .use(bodyParser.json())
   .use((req, res, next) => {
     next();
   })
-  .use('/', require('./routes'));
+  .use('/', isLoggedIn, require('./routes'));
 
-// catch the error when invalid ID is input.
 process.on('uncaughtException', (err, origin) => {
   console.log(
     process.stderr.fd,
